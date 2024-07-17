feda34070251a89a55177711999a33cbecd7be2c
joaquin <joaquinahuel93@gmail.com>
2024-07-17 10:09:17 -0300

    add res auth and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonCardsModule } from './pokemon-cards/pokemon-cards.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { SeedModule } from './seed/seed.module';
     PokemonCardsModule,
     CommonModule,
     SeedModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
